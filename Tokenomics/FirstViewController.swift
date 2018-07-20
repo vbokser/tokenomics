b2dd41432df79ff2459bca56bf6e683be036f70a
Vitaly Bokser <vbokser3@gatech.edu>
2018-07-20 12:29:37 -0400

    added ability to refresh data for the table tokens that are in the visible view
    working on color changes and % change over 1 hr 24 hrs etc

--- Tokenomics/FirstViewController.swift
+++ Tokenomics/FirstViewController.swift
@@ -9,56 +9,28 @@
 import UIKit
 import CryptoCurrencyKit
 
-class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+enum State {
+    case GatheringData
+    case DisplayData
+}
+
+class FirstViewController: UIViewController {
     @IBOutlet weak var cryptoTableView: UITableView!
     var cryptoArray = ["Bitcoin", "Ethereum", "Ripple"]
-
     var allTickers:[Ticker] = []
+    var dataTimer = Timer()
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allTickers.count
-    }
+    var indexPathsToReload = [IndexPath]()
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CRYPTO_CELL", for: indexPath)
-
-        if allTickers.count <= 0 {
-            return cell
-        }
-        
-        let name = cell.viewWithTag(1000) as! UILabel
-        let price = cell.viewWithTag(2000) as! UILabel
-//        cell.textLabel?.text = cryptoArray[indexPath.row]
-
-        let row = indexPath.row
-        name.text = allTickers[row].name //cryptoArray[indexPath.row]
-       
-        if let priceVal = allTickers[row].priceUSD {
-            price.text = "\(priceVal)"
-
-        } else {
-            price.text = "price unavailable"
-        }
-        
-        return cell
-    }
+    var currentState:State = State.DisplayData
+    var updateCounter:Int = 0
+    var numberOfTotalVisibleRows: Int = 0
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60.0
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let row = indexPath.row
-        
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.cryptoTableView.delegate = self
         fetchTickers()
-//        getCryptoData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -66,13 +38,76 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // Dispose of any resources that can be recreated.
     }
 
-    func getCryptoData() {
-        CryptoCurrencyKit.fetchTicker(coinName: "BitCoin", convert: .usd) { r in
-            switch r {
-            case .success(let bitCoin):
-                print(bitCoin)
-            case .failure(let error):
-                print(error)
+    @objc func getCryptoData() {
+        print("In getCryptoData")
+        //get data 1 ticker at a time
+        
+        if self.currentState == State.GatheringData {
+            return
+        }
+
+        var indexPaths = self.cryptoTableView.indexPathsForVisibleRows
+        
+        
+        if let indexPaths = indexPaths {
+            self.numberOfTotalVisibleRows = indexPaths.count
+//            print("Num visible rows = \(indexPaths.count)")
+//            print("index 1 = \(indexPaths.first!)")
+//            print("index last = \(indexPaths.last!)")
+//            for index in indexPaths {
+//                print"  seeing row = \(index.row)")
+//            }
+//            let currentCurrency:CryptoCurrencyKit.Money = .usd
+
+            for index in indexPaths {
+                var row = index.row
+                
+                CryptoCurrencyKit.fetchTicker(coinName: self.allTickers[row].id, convert: .usd) { r in
+                    switch r {
+                    case .success(let ticker):
+                        //print(ticker)
+                        print("--------")
+                        print(" ticker.name = \(ticker.name)")
+                        print(" index of ticker = \(self.allTickers.index(of: ticker))")
+                        if let index = self.allTickers.index(of: ticker) {
+                            print(" ticker found for index = \(self.allTickers[index])")
+                            if ticker.id == self.allTickers[index].id {
+                                //ok we have the same ticker thats now updated
+                                self.allTickers[index] = ticker
+                                
+                                
+//                                UIView.setAnimationsEnabled(false)
+//                                self.cryptoTableView.beginUpdates()
+                                
+                                let reloadIndexPath = IndexPath(item: Int(index), section: 0)
+                                self.indexPathsToReload.append(reloadIndexPath)
+
+//                                [self.tableView reloadRowsAtIndexPaths:[NSArray arrayWithObjects:indexPath, nil]
+//                                    withRowAnimation:UITableViewRowAnimationNone];
+//                                self.cryptoTableView.endUpdates()
+//                                UIView.setAnimationsEnabled(true)
+
+                                self.updateCounter += 1
+                                self.currentState = State.GatheringData
+                                if self.updateCounter >= self.numberOfTotalVisibleRows {
+                                    self.currentState = State.DisplayData
+                                    self.updateCounter = 0
+                                
+                                    UIView.setAnimationsEnabled(false)
+                                    self.cryptoTableView.beginUpdates()
+
+                                    self.cryptoTableView.reloadRows(at: self.indexPathsToReload, with: .none)
+                                    
+                                    self.cryptoTableView.endUpdates()
+                                    UIView.setAnimationsEnabled(true)
+                                }
+                            }
+                        }
+
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
             }
         }
     }
@@ -88,6 +123,10 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
                     self.allTickers.append(ticker)
                 }
                 self.cryptoTableView.reloadData()
+
+                //setup timer if we fetched all the tickers
+                self.dataTimer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.getCryptoData), userInfo: nil, repeats: true)
+
             case .failure(let error):
                 print(error)
             }
@@ -95,5 +134,51 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
 
 
+}
+
+extension FirstViewController: UITableViewDelegate, UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allTickers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CRYPTO_CELL", for: indexPath)
+        
+        if allTickers.count <= 0 {
+            return cell
+        }
+        
+        let name = cell.viewWithTag(1000) as! UILabel
+        let price = cell.viewWithTag(2000) as! UILabel
+        //        cell.textLabel?.text = cryptoArray[indexPath.row]
+        
+        let row = indexPath.row
+        name.text = allTickers[row].name //cryptoArray[indexPath.row]
+        
+        if let priceVal = allTickers[row].priceUSD {
+            price.text = "\(priceVal)"
+            
+        } else {
+            price.text = "price unavailable"
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let row = indexPath.row
+        print("Selected Row = \(row)")
+    }
+    
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
 }
 
